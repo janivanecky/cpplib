c57952ee1aafedd03dcae6a3f465e40e151f24ab
janivanecky <jan.ivanecky@hotmail.com>
2020-05-08 17:34:32 -0700

    Move stack to memory.cpp + remove asserts from memory.cpp

--- memory.cpp
+++ memory.cpp
@@ -1,14 +1,83 @@
 #include "memory.h"
-#include "stack.h"
+#include <cstring>
+
+struct Stack
+{
+    StackAllocatorState *data;
+    uint32_t top;
+    uint32_t size;
+};
+
+// `stack` namespace handles operations on Stack.
+namespace stack
+{
+    // Initialize stack and allocate initial memory necessary for stack of size `size`
+    void init(Stack *stack, uint32_t size)
+    {
+        stack->size = size;
+        stack->top = 0;
+        stack->data = memory::alloc_heap<StackAllocatorState>(size);
+    }
+
+    // Return an initialized stack of size `size`
+    Stack get(uint32_t size)
+    {
+        Stack stack;
+        stack::init(&stack, size);
+        if(!stack.data) {
+            stack.size = 0;
+        }
+        return stack;
+    }
+
+    // Reset the stack
+    // Does not free any memory
+    void reset(Stack *stack)
+    {
+        stack->top = 0;
+    }
+
+    // Push a new item from stack
+    void push(Stack *stack, StackAllocatorState item)
+    {
+        // In case stack is full, reallocate
+        if(stack->size == stack->top)
+        {
+            StackAllocatorState *new_mem = memory::alloc_heap<StackAllocatorState>(stack->size * 2);
+            // Maybe not the best way to handle the problem?
+            if(!new_mem) {
+                return;
+            }
+            memcpy(new_mem, stack->data, sizeof(StackAllocatorState) * stack->size);
+            memory::free_heap(stack->data);
+            stack->data = new_mem;
+            stack->size *= 2;
+        }
+
+        stack->data[stack->top++] = item;
+    }
+
+    // Pop an item from stack
+    StackAllocatorState pop(Stack *stack)
+    {
+        // Cannot pop if no items in stack
+        if(stack->top == 0){
+            return StackAllocatorState();
+        } 
+        return stack->data[stack->top--];
+    }
+}
 
 static StackAllocator allocator_temp = memory::get_stack_allocator(MEGABYTES(10));
-static Stack<StackAllocatorState> temp_state_stack = stack::get<StackAllocatorState>(10);
+static Stack temp_state_stack = stack::get(10);
 
 StackAllocator memory::get_stack_allocator(uint32_t size)
 {
     StackAllocator allocator = {};
     allocator.storage = malloc(size);
-    assert(allocator.storage);
+    if(!allocator.storage) {
+        return allocator;
+    }
     allocator.size = size;
     return allocator;
 }
